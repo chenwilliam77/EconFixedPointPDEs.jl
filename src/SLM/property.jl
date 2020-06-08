7b0d18afe3778e581ba037f94de6c0df9bb86217
William Chen <chenwilliamy77@gmail.com>
2020-06-07 22:35:25 -0400

    Move default_slm_kwargs! inside property.jl

--- src/SLM/property.jl
+++ src/SLM/property.jl
@@ -2,29 +2,44 @@
 
 """
 ```
-default_slm_kwargs!(kwargs)
+default_slm_kwargs!
 ```
 
-populates the kwargs dictionary with default values.
+adds the default keyword arguments for an SLM object.
 """
-function default_slm_kwargs!(kwargs::Dict{Symbol, Any})
+function default_slm_kwargs!(kwargs::Dict)
 
+    # degree
     if !haskey(kwargs, :degree)
         kwargs[:degree] = 3
     end
 
+    # # issorted
+    # if !haskey(kwargs, :issorted)
+    #     kwargs[:issorted] = false
+    # end
+
+    # scaling
+    if !haskey(kwargs, :scaling)
+        kwargs[:scaling] = true
+    end
+
+    # knots
     if !haskey(kwargs, :knots)
         kwargs[:knots] = 6
     end
 
-    if !haskey(kwargs, :scaling)
-        kwargs[:scaling] = on
-    end
-
+    # C2 function
     if !haskey(kwargs, :C2)
         kwargs[:C2] = true
     end
 
+    # Regularization parameter
+    if !haskey(kwargs, :λ)
+        kwargs[:λ] = 1e-4
+    end
+
+    # Monotonicity
     if !haskey(kwargs, :increasing)
         kwargs[:increasing] = false
     end
@@ -33,6 +48,32 @@ function default_slm_kwargs!(kwargs::Dict{Symbol, Any})
         kwargs[:decreasing] = false
     end
 
+    if !haskey(kwargs, :increasing_intervals)
+        kwargs[:increasing_intervals] = Matrix{Float64}(undef, 0, 0)
+    end
+
+    if !haskey(kwargs, :decreasing_intervals)
+        kwargs[:decreasing_intervals] = Matrix{Float64}(undef, 0, 0)
+    end
+
+    # Curvature
+    if !haskey(kwargs, :concave_up)
+        kwargs[:concave_up] = false
+    end
+
+    if !haskey(kwargs, :concave_down)
+        kwargs[:concave_down] = false
+    end
+
+    if !haskey(kwargs, :concave_up_intervals)
+        kwargs[:concave_up_intervals] = Matrix{Float64}(undef, 0, 0)
+    end
+
+    if !haskey(kwargs, :concave_down_intervals)
+        kwargs[:concave_down_intervals] = Matrix{Float64}(undef, 0, 0)
+    end
+
+    # End point values
     if !haskey(kwargs, :left_value)
         kwargs[:left_value] = NaN
     end
@@ -41,13 +82,21 @@ function default_slm_kwargs!(kwargs::Dict{Symbol, Any})
         kwargs[:right_value] = NaN
     end
 
-    if !haskey(kwargs, :concave_up)
-        kwargs[:concave_up] = false
+    # Global min and max
+    if !haskey(kwargs, :min_value)
+        kwargs[:min_value] = NaN
     end
 
-    if !haskey(kwargs, :concave_down)
-        kwargs[:concave_down] = false
+    if !haskey(kwargs, :max_value)
+        kwargs[:max_value] = NaN
     end
+
+    if !haskey(kwargs, :min_max_sample_points)
+        kwargs[:min_max_sample_points] = [.017037, .066987, .1465, .25, .37059,
+                                          .5, .62941, .75, .85355, .93301, .98296]
+    end
+
+    return kwargs
 end
 
 """
