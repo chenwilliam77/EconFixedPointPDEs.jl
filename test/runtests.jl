fbaf88a7c0431e472324b183b6cfa5cf5a884a92
William Chen <chenwilliamy77@gmail.com>
2020-06-26 10:32:22 -0400

    Update runtests file.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,29 @@ using SafeTestsets
 
 include("util.jl")
 
-# solve/
-include("solve/solve_nojump.jl")
-
-# models/
-include("models/li2020.jl")
-include("models/nojump_eqm.jl")
-
-# SLM/
+## SLM/
 include("SLM/default_slm_kwargs.jl")
 include("SLM/util.jl")
 include("SLM/property.jl")
 include("SLM/solve_slm_system.jl")
 include("SLM/slm.jl")
 include("SLM/eval.jl")
+
+## solve/
+include("solve/init_derivatives.jl")
+include("solve/solve_nojump.jl")
+include("solve/solve.jl")
+
+## models/
+
+# Li2020
+include("models/li2020.jl")
+include("models/li2020/nojump_eqm.jl")
+include("models/li2020/eqcond.jl")
+include("models/li2020/augment_variables.jl")
+include("models/li2020/solve.jl")
+
+
 
 # const GROUP = get(ENV, "GROUP", "All")
 # const is_APPVEYOR = Sys.iswindows() && haskey(ENV,"APPVEYOR")
