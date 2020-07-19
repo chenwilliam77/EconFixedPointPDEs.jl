d85f8510f438b8b241f676e39b134e033af9f09c
William Chen <chenwilliamy77@gmail.com>
2020-07-19 00:45:46 -0400

    Use initialize_nojump in Li2020 nojump eqm test.

--- test/models/li2020/nojump_eqm.jl
+++ test/models/li2020/nojump_eqm.jl
@@ -2,7 +2,7 @@ using Test, OrdinaryDiffEq, HDF5, ModelConstructors
 include(joinpath(dirname(@__FILE__), "../../../src/includeall.jl"))
 
 m = Li2020()
-stategrid, _ = initialize!(m)
+stategrid, _ = initialize_nojump!(m)
 ode_f, ode_callback = eqcond_nojump(m)
 tspan = (stategrid[:w][1], stategrid[:w][end])
 θ = parameters_to_named_tuple(map(x -> m.parameters[m.keys[x]], get_setting(m, :nojump_parameters)))
